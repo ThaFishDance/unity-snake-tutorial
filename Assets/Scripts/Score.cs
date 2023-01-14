d9a6f1ccd2da13202963e34bfff7755b1f8bb391
ThaFishDance <chuffman91@gmail.com>
2023-01-14 09:15:11 -0600

    Add score counter; remember future huff transform.find does work well

---
+++ Assets/Scripts/Score.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Score : MonoBehaviour
+{
+    private Text scoreText;
+    
+
+
+    private void Awake()
+    {
+        scoreText = GameObject.FindGameObjectWithTag("Score").GetComponent<Text>();
+    }
+
+    private void Update()
+    {
+        scoreText.text = "Score: " + Snake.GetScore().ToString();
+    }
+}
--- Assets/Scripts/Snake.cs
+++ Assets/Scripts/Snake.cs
@@ -1,14 +1,20 @@
 ﻿using System.Collections.Generic;
 using UnityEngine;
 
-[RequireComponent(typeof(BoxCollider2D))]
+[RequireComponent(typeof (BoxCollider2D))]
 public class Snake : MonoBehaviour
 {
-    private List<Transform> segments = new List<Transform>();
+    public List<Transform> segments = new List<Transform>();
+
     public Transform segmentPrefab;
+
     public Vector2 direction = Vector2.right;
+
     private Vector2 input;
+
     public int initialSize = 4;
+
+    public static int score;
 
     private void Start()
     {
@@ -20,18 +26,34 @@ public class Snake : MonoBehaviour
         // Only allow turning up or down while moving in the x-axis
         if (direction.x != 0f)
         {
-            if (Input.GetKeyDown(KeyCode.W) || Input.GetKeyDown(KeyCode.UpArrow)) {
+            if (Input.GetKeyDown(KeyCode.W) || Input.GetKeyDown(KeyCode.UpArrow)
+            )
+            {
                 input = Vector2.up;
-            } else if (Input.GetKeyDown(KeyCode.S) || Input.GetKeyDown(KeyCode.DownArrow)) {
+            }
+            else if (
+                Input.GetKeyDown(KeyCode.S) ||
+                Input.GetKeyDown(KeyCode.DownArrow)
+            )
+            {
                 input = Vector2.down;
             }
         }
-        // Only allow turning left or right while moving in the y-axis
-        else if (direction.y != 0f)
+        else // Only allow turning left or right while moving in the y-axis
+        if (direction.y != 0f)
         {
-            if (Input.GetKeyDown(KeyCode.D) || Input.GetKeyDown(KeyCode.RightArrow)) {
+            if (
+                Input.GetKeyDown(KeyCode.D) ||
+                Input.GetKeyDown(KeyCode.RightArrow)
+            )
+            {
                 input = Vector2.right;
-            } else if (Input.GetKeyDown(KeyCode.A) || Input.GetKeyDown(KeyCode.LeftArrow)) {
+            }
+            else if (
+                Input.GetKeyDown(KeyCode.A) ||
+                Input.GetKeyDown(KeyCode.LeftArrow)
+            )
+            {
                 input = Vector2.left;
             }
         }
@@ -40,14 +62,16 @@ public class Snake : MonoBehaviour
     private void FixedUpdate()
     {
         // Set the new direction based on the input
-        if (input != Vector2.zero) {
+        if (input != Vector2.zero)
+        {
             direction = input;
         }
 
         // Set each segment's position to be the same as the one it follows. We
         // must do this in reverse order so the position is set to the previous
         // position, otherwise they will all be stacked on top of each other.
-        for (int i = segments.Count - 1; i > 0; i--) {
+        for (int i = segments.Count - 1; i > 0; i--)
+        {
             segments[i].position = segments[i - 1].position;
         }
 
@@ -63,36 +87,49 @@ public class Snake : MonoBehaviour
     {
         Transform segment = Instantiate(segmentPrefab);
         segment.position = segments[segments.Count - 1].position;
-        segments.Add(segment);
+        segments.Add (segment);
     }
 
     public void ResetState()
     {
+        score = 0;
         direction = Vector2.right;
         transform.position = Vector3.zero;
 
         // Start at 1 to skip destroying the head
-        for (int i = 1; i < segments.Count; i++) {
+        for (int i = 1; i < segments.Count; i++)
+        {
             Destroy(segments[i].gameObject);
         }
 
         // Clear the list but add back this as the head
         segments.Clear();
-        segments.Add(transform);
+        segments.Add (transform);
 
         // -1 since the head is already in the list
-        for (int i = 0; i < initialSize - 1; i++) {
+        for (int i = 0; i < initialSize - 1; i++)
+        {
             Grow();
         }
     }
 
     private void OnTriggerEnter2D(Collider2D other)
     {
-        if (other.gameObject.CompareTag("Food")) {
+        if (other.gameObject.CompareTag("Food"))
+        {
+            Debug.Log("growing");
+            score++;
+            Debug.Log("score: " + score);
             Grow();
-        } else if (other.gameObject.CompareTag("Obstacle")) {
+        }
+        else if (other.gameObject.CompareTag("Obstacle"))
+        {
             ResetState();
         }
     }
 
+    public static int GetScore()
+    {
+        return score;
+    }
 }
